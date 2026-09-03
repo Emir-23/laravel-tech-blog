81d56346f39d92b6ea0d5112e3aafd7b1095b115
Emir-23 <m.emir2300@gmail.com>
2026-09-03 14:38:05 +0300

    ilk commit: proje altyapisi

---
+++ app/Models/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use ılluminate\Database\Eloquent\Relations\HasMany;
+class Category extends Model
+{
+    protected $fillable = ['name', 'slug'];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+}
